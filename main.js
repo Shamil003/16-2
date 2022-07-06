f949c9d55fff8987bc4c9338e6335f332c149f16
SH-L <you@example.com>
2022-07-06 22:01:14 +0600

    dz

---
+++ main.js
@@ -0,0 +1,61 @@
+const a = "a";//не изменяемый , string
+
+let b = 45;//изменяемый  , number
+
+const bool = true; // boolen
+const boo2 = false;
+
+const obj = {
+    key: 'value',
+    username: 'sara',
+    data: {
+        first_name: "Jack",
+        last_name: "barbaro"
+    }
+}
+
+//console.log(obj.data.last_name)
+
+const arr = ["Shamil", true, false, 25]
+// console.log(arr[1])
+
+const arr2 = [
+    {
+        key: 'value'
+    }, 
+    {
+        key: 'value1'
+    },
+    {
+        key: 'value2'
+    },
+]
+//console.log(arr2[2].key)
+
+//const name = prompt ("Как вас зовут?");
+// console.log(name)
+
+// const age = alert ("Вам ещё нет 18!")
+
+// const first_number = prompt("1-nember")
+// const last_number = prompt("2-nember")
+// 
+// if (first_number > last_number){
+    // console.log(first_number+ ">" + last_number)
+// } else if (last_number > first_number){
+    // console.log(first_number+ "<" + last_number)
+// }
+// else {
+    // console.log(first_number+ "=" + last_number)
+// }
+
+// const age = prompt ("Сколько вам лет?")
+
+// if (age < 18){
+    // console.log("вам нельза входить")
+// } else if (age > 18){
+    // console.log("Вам можно)")
+// } else {
+    // console.log("Поживи еще годик")
+// }
+
